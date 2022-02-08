7ad5826a53144eb53f223989cc8a9f1edefd8d9c
Phil Price <pnprice@creekcats.com>
2022-02-08 11:28:42 -0800

    try second source of covid data for better county coverage

--- covidexample.R
+++ covidexample.R
@@ -44,8 +44,11 @@ covid_dat = covid_dat %>%
   mutate(fips = 1000*FIPS.State+FIPS.County) %>%
   filter(Indicator %in% 'Distribution of COVID-19 deaths (%)')
   
+cov_dat_2 = read.csv('./data/Provisional_COVID-19_Death_Counts_in_the_United_States_by_County.csv', 
+                     header = T, as.is=T)
+cov_dat_2 = cov_dat_2 %>% mutate(fips = FIPS.County.Code)
 
-dat = left_join(demog_dat, covid_dat, by = 'fips' ) 
+dat = left_join(demog_dat, cov_dat_2, by = 'fips' ) 
 dat = dat %>% mutate(
   u5frac = UNDER5_TOT / POPESTIMATE,
   age513frac = AGE513_TOT / POPESTIMATE, 
@@ -64,13 +67,29 @@ dat = dat %>% mutate(
   age7579frac = AGE7579_TOT / POPESTIMATE,
   age8084frac = AGE8084_TOT / POPESTIMATE,
   age85plusfrac = AGE85PLUS_TOT / POPESTIMATE,
-  totdeathfrac = Total.deaths / POPESTIMATE,
-  covdeathfrac = COVID.19.Deaths / POPESTIMATE,
+  totdeathfrac = Deaths.from.All.Causes / POPESTIMATE,
+  covdeathfrac = Deaths.involving.COVID.19 / POPESTIMATE,
   covdeathpct = 100*covdeathfrac
 )
 
-plot_usmap(data = dat, values = 'covdeathpct', regions='counties') +  
-  scale_fill_continuous(low = 'white', high = 'red', name = "Pct of pop. killed by COVID", label = scales::comma)
+# One county is an outlier, with a COVID death rate of 3.8% of the population, almost double the 
+# second-highest county (1.98%). Next after that are 1.8%, 1.7%, 1.2%.  Those four counties over 1.2%
+# really throw off the color scale of the map. Create another variable that chops those down to
+# lower values.
+dat$covdeathpcttrunc = ifelse(dat$covdeathpct > 1.2, 1.3, dat$covdeathpct)
+
+
+plot_usmap(data = dat, values = 'covdeathpcttrunc', regions='counties') +  
+  #scale_fill_continuous(low = 'white', high = 'red', name = "Pct of pop. killed by COVID", label = scales::comma)
+  scale_colour_gradient(
+    low = "white",
+    high = "red",
+    space = "Lab",
+    na.value = "grey50",
+    guide = "colourbar",
+    aesthetics = "fill"
+  )
+
 
 
 plot_usmap(data = dat, values = 'COVID.19.Deaths', regions='counties') +  
