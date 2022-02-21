d2e598b51504f6ed22fecd396744fcef2512b811
Phil Price <pnprice@creekcats.com>
2022-02-20 23:58:56 -0800

    oops, added radiation

--- particle_simulate.R
+++ particle_simulate.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(ggplot2)
 library(gridExtra)
 
 inregion = function(x,y) {
@@ -6,7 +7,7 @@ return ((x^2+y^2 < 1) | (abs(x) < 0.5 & y > -2.0 & y < 0.0))
 } 
 
 
-N = 100000
+N = 1000000
 
 # Initialize the dataframe
 partdf = data.frame(active = rep(T,N),
@@ -15,8 +16,11 @@ partdf = data.frame(active = rep(T,N),
                       E  = rexp(n=N, rate = 1.0))
   
 n_active = sum(partdf$active)
+n_count = 0
+
 while (n_active > 0) {
-  print(n_active)
+  print(paste(n_active, sum(partdf$E)))
+  # print(n_active)
   # Move the active particles
   partdf = partdf %>% 
     mutate(
@@ -24,23 +28,30 @@ while (n_active > 0) {
       y = y + active*rnorm(n=N, mean=0, sd=0.1),
     )
   
-  # For each particle: if this particle is selected to lose energy,
-  # which active particle will we transfer the energy to?
-  transfer_to = sample(which(partdf$active), N, replace = T)
+  samp1 = sample(which(partdf$active)) # index of active particles in random order
+  samp2 = sample(which(partdf$active))
+  rand = runif(n=length(samp1)) 
+  radiate = samp1[rand >= 0.5]  # some particles lose energy by radiation.
+  partdf$E[radiate] = 0.9*partdf$E
   
-  # Transfer energy from some particles to others.
-  ok_transfer = partdf$active & (runif(n=N) < 0.5)
-  e_to_transfer = 0.1*partdf$E[ok_transfer]
-  partdf$E[transfer_to[ok_transfer]] = partdf$E[transfer_to[ok_transfer]] + e_to_transfer
-  partdf$E[ok_transfer] = partdf$E[ok_transfer] - e_to_transfer
+  transfer_from = samp1[rand < 0.5] # some particles transfer energy to others
+  transfer_to = samp2[rand < 0.5]
+  e_to_transfer = 0.1*partdf$E[transfer_from]
+  partdf$E[transfer_to] = partdf$E[transfer_to] + e_to_transfer
+  partdf$E[transfer_from] = partdf$E[transfer_from] - e_to_transfer
+  
+  
+  partdf$E[]
+  
   
   partdf$active[!inregion(partdf$x, partdf$y)] = F
   n_active = sum(partdf$active)
 }
 
-p1 = ggplot(partdf, aes(x=x,y=y)) + geom_point()
+p1 = ggplot(partdf, aes(x=x,y=y)) + geom_point(aes(color=E))
 p2 = ggplot(partdf) + geom_histogram(aes(x = E )) + scale_x_log10()
 
 grid.arrange(p1, p2, nrow = 1)
 
+print(paste("Total energy is", sum(partdf$E)))
 print(paste("99.95 percentile of energy is:", quantile(partdf$E,0.9995)))
