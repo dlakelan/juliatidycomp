f4cc4c85e0f9f32dcbd8a4739560e7cf1608862d
Daniel Lakeland <dlakelan@street-artists.org>
2022-10-07 18:11:42 -0700

    add the card trick simulation

---
+++ cardtricksim.jl
@@ -0,0 +1,37 @@
+using Random
+
+suit = [collect(1:9); [1, 1, 1, 1]]
+deck = repeat(suit,4)
+
+function walk(deck,start)
+    while start + deck[start] <= length(deck)
+        start = start + deck[start]
+    end
+    return start
+end
+
+function couples!(deck)
+    shuffle!(deck)
+    starts = rand(1:5,2)
+    end1 = walk(deck,starts[1])
+    end2 = walk(deck,starts[2])
+    return end1 == end2
+end
+
+
+function sim(N,deck)
+    successes = 0.0
+    for i in 1:N
+        successes += couples!(deck)
+    end
+    rate = successes / N
+    v = rate * (1-rate)*N
+    s = sqrt(v)
+    se = s/N
+    return (lb = rate - 1.96*se, p = rate, ub=rate+1.96*se)
+end
+
+sim(10,deck)
+@time sim(1_000_000,deck)
+
+
